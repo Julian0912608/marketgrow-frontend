55f924c28006cca141c01a3c50fbcf0a2c57257f
Julian0912608 <juligoventures@gmail.com>
2026-04-29 18:03:58 +0200

    Update index.ts

--- components/marketing/index.ts
+++ components/marketing/index.ts
@@ -1,1 +1,13 @@
-export { ProductPerformance, MarketingInsights, GrowthOpportunities, AdCreative, PlatformBar, Pricing, Testimonials, FAQ, CTA, Footer } from './sections';
+export { Navbar }   from './Navbar';
+export { Hero }     from './Hero';
+export {
+  PainPoints,
+  HowItWorks,
+  AIContentStudio,
+  AIAds,
+  PlatformBar,
+  Pricing,
+  FAQ,
+  CTA,
+  Footer,
+} from './sections';
